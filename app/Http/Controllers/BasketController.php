ecfa4c28c8678ba4d208e82b062f73ec19ea085f
sivan2203 <sivan9458@gmail.com>
2021-03-31 23:43:16 +0700

    add metods in BasketController

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -2,16 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
-class BasketController extends Controller
-{
+class BasketController extends Controller{
 
     public function basket(){
-        return view('basket');
+        $orderId = session('orderId');
+        if(!is_null($orderId)){
+            $order = findOrFail($orderId);
+        }
+        return view('basket', compact('order'));
     }
 
     public function basketPlace(){
         return view('order');
+    }
+
+    public function basketAdd($productId){
+        $orderId = session('orderId');
+        if(is_null($orderId)){
+            $order = Order::create()->id;
+            session(['orderId' => $order->id]);
+        } else {
+            $order = Order::find($orderId);
+        }
+        $order->products()->attach($productId);
+
+        return view('basket', compact('order'));
     }
 }
--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -7,10 +7,10 @@
             <h3>{{$product->name}}</h3>
             <p>{{$product->price}} руб</p>
             <p>
-            <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
+            <form action="{{route('basket-add', $product)}}" method="POST">
                 <button type="submit" class="btn btn-primary" role="button">В корзину</button>
                 <a href="{{route('product', [$product->category->code, $product->code])}}" class="btn btn-default" role="button">Подробнее</a>
-                <input type="hidden" name="_token" value="Gefc7YGBmO5XcmYk45IFoJFAc7knXFJpBqmxvtqo">
+                @csrf
             </form>
             </p>
         </div>
